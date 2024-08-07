e7b29efed88cc1ded2210771494485a1afdf0206
fewtarius <fewtarius@steamfork.org>
2024-08-07 10:39:44 +0000

    Updates.

--- docker/build.sh
+++ docker/build.sh
@@ -4,20 +4,23 @@
 ### Create the persistent volume if it doesn't exist.
 ###
 
-docker volume inspect appdata 2>&1 >/dev/null
+docker volume inspect appdata >/dev/null 2>&1
 if [ ! $? = 0 ]
 then
-  docker volume create appdata
+  docker volume create appdata >/dev/null 2>&1
 fi
 
 ###
 ### Build the image
 ###
 
-docker image build -t photonbbs .
+docker buildx build --tag fewtarius/photonbbs .
 
 ###
 ### Describe how to start the container.
 ###
 
-echo -e "\n\nTo use this container, run:\n\ndocker container run -dti --net host --device=/dev/tty0 -v appdata:/appdata:rw -v /dev:/dev -v /lib/modules:/lib/modules --privileged photonbbs"
+if [ $? = 0 ]
+then
+  echo -e "\n\nTo use this container, run:\n\ndocker container run -dti --net host --device=/dev/tty0 -v appdata:/appdata:rw -v /dev:/dev -v /lib/modules:/lib/modules --cgroup-parent=docker.slice --cgroupns private --privileged fewtarius/photonbbs"
+fi
